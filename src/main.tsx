8970013ebdd4ddd713dd4256943a0037f6f78524
RickDelgado <ra.delgadonava@gmail.com>
2025-12-31 08:15:02 -0600

    se agrega FocusScreen component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import PokemonPage from "./03-examples/PokemonPage";
+import FocusScreen from "./useRef/FocusScreen";
+// import PokemonPage from "./03-examples/PokemonPage";
 // import { TrafficLightWithEffect } from "./02-useEffect.tsx/TrafficLightWithEffect";
 // import { TrafficLightWithHook } from "./02-useEffect.tsx/TrafficLightWithHook";
 // import { HooksApp } from "./HooksApp";
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>
 );
---
+++ src/useRef/FocusScreen.tsx
@@ -0,0 +1,11 @@
+const FocusScreen = () => {
+  return (
+    <div className="bg-gradient flex gap-4">
+      <h1 className="text-2xl font-thin text-white">Focus Screen</h1>
+
+      <input type="text" className="bg-white text-black px-4 py-2 rounded-md" />
+    </div>
+  );
+};
+
+export default FocusScreen;
