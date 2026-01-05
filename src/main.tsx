e0ec2309188097d7042dec801629031b5d4d940c
RickDelgado <ra.delgadonava@gmail.com>
2026-01-05 17:01:31 -0600

    se configura seccion 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import FocusScreen from "./useRef/FocusScreen";
+import { TasksApp } from "./05-useReducer/TaskApp";
+// import FocusScreen from "./useRef/FocusScreen";
 // import PokemonPage from "./03-examples/PokemonPage";
 // import { TrafficLightWithEffect } from "./02-useEffect.tsx/TrafficLightWithEffect";
 // import { TrafficLightWithHook } from "./02-useEffect.tsx/TrafficLightWithHook";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>
 );
