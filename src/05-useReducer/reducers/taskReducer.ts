a3d9a61b8251bdcfc28c360055b64ef901925744
RickDelgado <ra.delgadonava@gmail.com>
2026-01-15 15:37:08 -0600

    se agrega logica de reducer segun el valor del action

--- src/05-useReducer/reducers/taskReducer.ts
+++ src/05-useReducer/reducers/taskReducer.ts
@@ -20,5 +20,43 @@ export const taskReducer = (
   state: TaskState,
   action: TaskAction
 ): TaskState => {
-  return state;
+  switch (action.type) {
+    case "ADD_TODO":
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.length + 1,
+        pending: state.pending + 1,
+      };
+    case "TOGGLE_TODO":
+      const updatedTodos = state.todos.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
+      };
+    case "DELETE_TODO":
+      const filteredTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload
+      );
+      return {
+        ...state,
+        todos: filteredTodos,
+        length: filteredTodos.length,
+        completed: filteredTodos.filter((todo) => todo.completed).length,
+        pending: filteredTodos.filter((todo) => !todo.completed).length,
+      };
+    default:
+      return state;
+  }
 };
